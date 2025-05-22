42fb5155ca460b46b513e921e8bb8a1436eaac4a
Omaya404 <omaya.battour@gmail.com>
2025-05-22 16:55:45 +0200

    bonnereponse

--- src/components/Questions/QuestionUne.tsx
+++ src/components/Questions/QuestionUne.tsx
@@ -1,23 +1,17 @@
-import "./QuestionUne.css"
+import "./QuestionUne.css";
 function QuestionUne() {
+	return (
+		<>
+			<div className="variable">
+				<h1>🟢 Q1 : Qu’est-ce qu’une variable ?</h1>
 
-    return (
-        <>
-            <div className="variable">
-
-                <h1>🟢 Q1 : Qu’est-ce qu’une variable ?
-                </h1>
-
-                <p>
-                    🍉 Une boîte qui peut contenir une valeur
-                    🍋 Une fonction spéciale
-                    🍇 Un nom de fichier
-                    🥝​ Une erreur dans le code
-
-                </p>
-            </div>
-        </>
-    )
+				<p>
+					🍉 Une boîte qui peut contenir une valeur 🍋 Une fonction spéciale 🍇
+					Un nom de fichier 🥝​ Une erreur dans le code
+				</p>
+			</div>
+		</>
+	);
 }
 
 //bonne reponse pastèque
