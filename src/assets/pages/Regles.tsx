ad605bce1d3b163b006c34053587343d65497a0b
jimmy <jimmy.balay97@gmail.com>
2025-05-22 15:15:28 +0200

    regle

---
+++ src/assets/pages/Regles.tsx
@@ -0,0 +1,14 @@
+function Regles() {
+    return (
+        <article>
+
+            <h1>😛 LES REGLES DE CODEMAN 😛</h1>
+            <p>CODEMAN DOIT TROUVER LA BONNE REPONSE POUR ACCUUMULER LE MAXIMUM DE POINTS. 💯
+                FAIT ATTENTION A CE QUE CODEMAN NE SE FASSE PAS CHOPER PAR LES BADBUGS SINON TU PERDERAS DES POINTS !
+                BON COURAGE.</p>
+
+        </article>
+    )
+}
+
+export default Regles
